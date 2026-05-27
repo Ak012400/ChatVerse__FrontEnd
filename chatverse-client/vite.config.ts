2b884d951e9f84f64d8b30d5ad570022e8342b99
Ak012400 <arunkumaramba84@gmail.com>
2026-05-27 06:12:59 +0530

    config changes

--- chatverse-client/vite.config.ts
+++ chatverse-client/vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import checker from 'vite-plugin-checker' // Ye import add kiya
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(), 
+    tailwindcss(),
+    checker({ typescript: true }) // Ye checker plugin add kiya gaya hai
+  ],
   server: {
     proxy: {
       '/api': {
